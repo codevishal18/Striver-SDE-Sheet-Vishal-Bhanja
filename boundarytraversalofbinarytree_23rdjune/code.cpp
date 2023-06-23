59488c15fd49a5c8e0d018670a4ee9b55f688bd4
codevishal18 <93566289+codevishal18@users.noreply.github.com>
2023-06-23 17:10:01 +0000

    $ git commit -a

---
+++ boundarytraversalofbinarytree_23rdjune/code.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+void traverseLeft(TreeNode<int>* root,vector<int>&ans)
+{
+   if((root==NULL)||(root->left==NULL &&root->right==NULL))
+    return;
+
+   ans.push_back(root->data);
+
+   if(root->left)
+    traverseLeft(root->left,ans);
+   else
+    traverseLeft(root->right,ans);
+
+}
+
+void traverseLeaf(TreeNode<int>* root,vector<int>&ans)
+{
+      if(root==NULL)
+        return;
+
+      if(root->left==NULL && root->right==NULL)
+      {
+          ans.push_back(root->data);
+          return;
+      }
+
+      traverseLeaf(root->left,ans);
+      traverseLeaf(root->right,ans);
+
+}
+
+void traverseRight(TreeNode<int>* root,vector<int>&ans)
+{
+   if((root==NULL)||(root->left==NULL &&root->right==NULL))
+    return;
+
+   if(root->right)
+    traverseRight(root->right,ans);
+   else
+    traverseRight(root->left,ans);
+    ans.push_back(root->data);
+
+}
+
+vector<int> traverseBoundary(TreeNode<int>* root)
+{
+    vector<int>ans;
+    if(root==NULL)
+        return ans;
+
+    ans.push_back(root->data);
+    traverseLeft(root->left,ans);
+    traverseLeaf(root->left,ans);
+    traverseLeaf(root->right,ans);
+    traverseRight(root->right,ans);
+    return ans;
+
+}
